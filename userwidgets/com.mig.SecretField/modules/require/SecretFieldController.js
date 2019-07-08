706fb3a5769040bcb0f67093286ced7ee33a87b1
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-07-08 21:59:13 +0200

    Allow toggling secret visibility on and off

--- userwidgets/com.mig.SecretField/modules/require/SecretFieldController.js
+++ userwidgets/com.mig.SecretField/modules/require/SecretFieldController.js
@@ -1,8 +1,34 @@
 define(function() {
 
-	return {
-		constructor: function(baseConfig, layoutConfig, pspConfig) {
+	const EYE_OPEN = "\uF06E";
+	const EYE_CLOSED = "\uF070";
 
+	return {
+		toggleButton: function (button){
+			if(button.text === EYE_OPEN){
+				button.text = EYE_CLOSED;
+				this.view.textBox.secureTextEntry = false;
+			}
+			else{
+				button.text = EYE_OPEN;
+				this.view.textBox.secureTextEntry = true;
+			}
+		},
+
+		preShow: function(){
+			this.view.showButton.text = EYE_OPEN;
+		},
+
+		postShow: function(){
+			this.view.showButton.onTouchEnd = this.toggleButton;
+		},
+
+		onHide: function(){},
+
+		constructor: function(/*baseConfig, layoutConfig, pspConfig*/) {
+			this.view.preShow = this.preShow;
+			this.view.postShow = this.postShow;
+			this.view.onHide = this.onHide;
 		},
 		//Logic for getters/setters of custom properties
 		initGettersSetters: function() {
